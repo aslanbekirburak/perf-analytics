c3627d8ecaedd9a086567d78334cd2fe1dd8fa23
bekir16 <aslanbekirburak@gmail.com>
2020-11-13 00:49:47 +0300

    chore: change page load

--- public/perfanalytics.js
+++ public/perfanalytics.js
@@ -1,8 +1,11 @@
 
 
 window.onload = function() {
-const paintEntries = window.performance.getEntriesByType("paint")[0];
+const paintEntries = window.performance.getEntriesByType("paint")[1];
 const navigationEntries = performance.getEntriesByType("navigation")[0];
+
+console.log("paintEntries", paintEntries)
+
 //TTFB
 const ttfb = navigationEntries.responseStart - navigationEntries.requestStart;
 
@@ -19,12 +22,11 @@ const siteName = window.location.href;
 const data = JSON.stringify({
   site: siteName,
   ttfb: ttfb,
-  fcp: paintEntries && paintEntries.startTime,
+  fcp: paintEntries && paintEntries.startTime || 0,
   domload: domLoad,
   windowLoadEvents: windowLoadEvents
   })
   console.log("data",data)
-  // https://bba-performance-analytics.herokuapp.com
   fetch("https://bba-performance-analytics.herokuapp.com", {
     method: "POST",
     headers: {
